a1b49065829959888450755362a41c02968bc8d0
RomanKirichek <kirichek.roman0@gmail.com>
2023-11-13 18:33:48 +0300

    first changes in bot

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
 import telebot
-token='6720448392:AAGmJjOMLIzfy0OlNNJguIvP7LbS1V7NCfQ'
-bot=telebot.TeleBot(token)
-@bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id,"Привет")
-bot.infinity_polling()  
+from telebot import types
+bot = telebot.TeleBot("6720448392:AAGmJjOMLIzfy0OlNNJguIvP7LbS1V7NCfQ")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "wassap")
+
+markup = types.ReplyKeyboardMarkup(row_width=2)
+itembtn1 = types.KeyboardButton('a')
+itembtn2 = types.KeyboardButton('v')
+itembtn3 = types.KeyboardButton('d')
+markup.add(itembtn1, itembtn2, itembtn3)
+
+
+bot.infinity_polling()
