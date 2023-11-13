1b87949bf9653a0e97decf670b56348b41523dbb
RomanKirichek <kirichek.roman0@gmail.com>
2023-11-13 18:07:33 +0300

    first adding

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
-print('Hello world')
-print('hard to understand')
-print('idfdgdsgsg')
+import telebot
+token='6720448392:AAGmJjOMLIzfy0OlNNJguIvP7LbS1V7NCfQ'
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет")
+bot.infinity_polling()  
