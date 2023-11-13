073ea0e973b49cd5fa5786cf9e8742d2956ae405
RomanKirichek <kirichek.roman0@gmail.com>
2023-11-13 20:12:17 +0300

    butttons with saying

--- bot.py
+++ bot.py
@@ -9,13 +9,35 @@ bot = telebot.TeleBot('6720448392:AAGmJjOMLIzfy0OlNNJguIvP7LbS1V7NCfQ')
 def send_welcome(message):
     # Create a custom keyboard with buttons
     keyboard = types.ReplyKeyboardMarkup(row_width=2)
-    button1 = types.KeyboardButton('Button 1')
-    button2 = types.KeyboardButton('Button 2')
-    button3 = types.KeyboardButton('Button 3')
+    button1 = types.KeyboardButton('Найти объем сферы')
+    button2 = types.KeyboardButton('Найти площадь поверхности сферы')
+    button3 = types.KeyboardButton('Найти объем конуса')
+   
     keyboard.add(button1, button2, button3)
 
     # Send a welcome message with the custom keyboard
-    bot.reply_to(message, "Welcome to the bot! Please select an option:", reply_markup=keyboard)
+    bot.reply_to(message, "Этот бот поможет тебе решить эти формулы:", reply_markup=keyboard)
 
+# Handle button clicks
+@bot.message_handler(func=lambda message: message.text == 'Button 1')
+def handle_button1(message):
+    # Perform action for Button 1
+    bot.reply_to(message, "You pressed Button 1!")
+
+@bot.message_handler(func=lambda message: message.text == 'Button 2')
+def handle_button2(message):
+    # Perform action for Button 2
+    bot.reply_to(message, "You pressed Button 2!")
+
+@bot.message_handler(func=lambda message: message.text == 'Button 3')
+def handle_button3(message):
+    # Perform action for Button 3
+    bot.reply_to(message, "You pressed Button 3!")
+
+# Handle user messages
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    # Respond to user message
+    bot.reply_to(message, "You said: " + message.text)
 # Start the bot
 bot.polling()
