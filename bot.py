ac4701bfdfbb818c2181387bd166307385208441
RomanKirichek <kirichek.roman0@gmail.com>
2023-11-13 19:34:27 +0300

    add buttons

--- bot.py
+++ bot.py
@@ -1,12 +1,21 @@
 import telebot
 from telebot import types
-bot = telebot.TeleBot("6720448392:AAGmJjOMLIzfy0OlNNJguIvP7LbS1V7NCfQ")
 
-@bot.message_handler(commands=['start', 'help'])
+
+bot = telebot.TeleBot('6720448392:AAGmJjOMLIzfy0OlNNJguIvP7LbS1V7NCfQ')
+
+# Handle the /start command
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "wassap")
+    # Create a custom keyboard with buttons
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+    button1 = types.KeyboardButton('Button 1')
+    button2 = types.KeyboardButton('Button 2')
+    button3 = types.KeyboardButton('Button 3')
+    keyboard.add(button1, button2, button3)
 
+    # Send a welcome message with the custom keyboard
+    bot.reply_to(message, "Welcome to the bot! Please select an option:", reply_markup=keyboard)
 
-
-
-bot.infinity_polling()
+# Start the bot
+bot.polling()
